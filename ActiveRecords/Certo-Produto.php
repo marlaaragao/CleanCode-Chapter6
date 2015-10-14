06e2f5e5a2388f91625aecbd7080c57254b08ff8
Marla Aragao <vargas.marla90@gmail.com>
2015-10-14 14:07:04 -0300

    correcao

--- ActiveRecords/Certo-Produto.php
+++ ActiveRecords/Certo-Produto.php
@@ -1,43 +1,42 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
 class Produto extends ActiveRecords {
-    
+
     private $_nome;
     private $_preco;
     private $_tableName;
-    
+
     function __construct($nome, $preco) {
 
         $this->_nome = $nome;
         $this->_preco = $preco;
-        
+
         $this->_tableName = 'produtos';
         parent::__construct($this->_tableName);
     }
-	
-	function calculaLucro() {
+
+    function calculaLucro() {
         //Implementação
     }
-    
+
     function calculaDepreciacao($periodo) {
         //Implementação
     }
-            
+
     //Funções pertinentes da regra de negocio (...)
 }
 
 class Testa {
-	
-	function exemplo() {
-		$p = new Produto($_POST['nome'], $_POST['preco']);
-		$p->save($p);
-	}
-	
-}
 
+    function exemplo() {
+        $p = new Produto($_POST['nome'], $_POST['preco']);
+        $p->save($p);
+    }
+
+}
