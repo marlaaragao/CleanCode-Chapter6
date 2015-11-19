08e156f0161caa9acabfecf915d295799d7eeb64
Marla Aragao <vargas.marla90@gmail.com>
2015-11-19 09:40:58 -0200

    Correção

--- Algoritmos/BMHS.php
+++ Algoritmos/BMHS.php
@@ -1,40 +1,5 @@
 class BM {
-	function bmh() {
-
-        $i = 0;
-        $j = 0;
-        $k = 0;
-        $texto = 'testeteste';
-        $padrao = 'teste';
-        $m = strlen($padrao);
-        $n = strlen($texto);
-        $pular = array();
-        $tamanho_alfabeto = 28;
-        
-        for ($k = 0; $k < $tamanho_alfabeto; $k++) {
-            $pular[$k] = $m;
-        }
-        for ($k = 1; $k < $m; $k++) {
-            $pular[$padrao[$k - 1]] = $m - $k;
-        }
-        
-        $i = $m;
-        
-        while ($i <= $n) {
-            $k = $i;
-            $j = $m;
-            while($j > 0 && $texto[$k - 1] == $padrao[$j - 1]) {
-                $k--;
-                $j--;
-            }
-            if ($j == 0) {
-                echo 'Casamento na posição: ' . ($k) . '<br>';                
-            }
-            $i += $pular[$texto[$i - 1]];
-        }    
-    }
-    
-    
+	
     function bmhs() {
 
         $i = 0;
